4d6c00c60f212fad57fecf8b02560be3c9faade6
allonte <allankaoga1@gmail.com>
2025-08-21 03:46:14 -0700

    feat: enlarge header logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,7 +10,7 @@ const Header = () => {
           <img
             src="/murbanlogo.jpg"
             alt="Murban logo"
-            className="w-8 h-8 object-cover rounded"
+            className="w-12 h-12 object-contain"
           />
           <span className="font-semibold text-foreground">Total Energies Uganda</span>
         </div>
