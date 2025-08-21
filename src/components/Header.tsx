d4359fb57f09ca3e204ff880fee0f8d3a6f61acb
allonte <allankaoga1@gmail.com>
2025-08-21 05:05:59 -0700

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -12,7 +12,7 @@ const Header = () => {
             alt="Murban logo"
             className="w-16 h-16 object-contain rounded"
           />
-          <span className="font-bold text-foreground">Murban</span>
+          <span className="font-bold text-foreground">Murban limited</span>
         </div>
         <div className="flex flex-col items-end gap-2">
           <div className="flex items-center gap-4">
