b9f75f4ad048808077e8817a5e7d1c89b967617c
allonte <allankaoga1@gmail.com>
2025-08-26 10:38:07 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,14 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-
+// Force favicon update
+const favicon = document.querySelector('link[rel="icon"]') as HTMLLinkElement;
+if (favicon) {
+  favicon.href = '/murbanlogo.jpg';
+} else {
+  const newFavicon = document.createElement('link');
+  newFavicon.rel = 'icon';
+  newFavicon.href = '/murbanlogo.jpg';
+  document.head.appendChild(newFavicon);
+}
 createRoot(document.getElementById("root")!).render(<App />);
